aaccac8cd95ba9506849e15ebc48d2debcc0755a
eferm <emanuel@eferm.com>
2020-12-05 01:42:24 -0600

    day 5

--- aoc_2020/day_05.py
+++ aoc_2020/day_05.py
@@ -1,24 +1,14 @@
 from _utils import *
 
 inp = get_input(2020, 5)
-# print(inp[:80])
-
-# inp = """BFFFBBFRRR
-# FFFBBBFRRR
-# BBFFBBFRLL
-# """
 
 seats = inp.strip().split("\n")
-print(seats)
-
 seat_ids = []
 
 for seat in seats:
     x_half = 8
     y_half = 128
-    print(seat)
     y_min, y_max = 0, 127
-    # print(seat[:7], seat[7:])
 
     for y in seat[:7]:
         y_half = y_half // 2
@@ -37,9 +27,12 @@ for seat in seats:
 
     row = y_min
     col = x_min
-
-    # print(row, col)
-
     seat_ids.append(row * 8 + col)
 
 print(max(seat_ids))
+
+
+# part 2
+for i, k in zip(sorted(seat_ids)[0::2], sorted(seat_ids)[1::2]):
+    if i + 1 != k:
+        print((i + k) // 2)
