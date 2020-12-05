40323a45f035642ad8bc242521e2ec057135e180
eferm <emanuel@eferm.com>
2020-12-05 15:30:49 -0600

    day 5 cleanup

--- aoc_2020/day_05.py
+++ aoc_2020/day_05.py
@@ -1,38 +1,19 @@
 from _utils import *
 
 inp = get_input(2020, 5)
-
 seats = inp.strip().split("\n")
-seat_ids = []
 
+# part 1
+ids = []
 for seat in seats:
-    x_half = 8
-    y_half = 128
-    y_min, y_max = 0, 127
+    y = seat[:7].replace("F", "0").replace("B", "1")
+    x = seat[7:].replace("L", "0").replace("R", "1")
+    ids.append(int(y, 2) * 8 + int(x, 2))
 
-    for y in seat[:7]:
-        y_half = y_half // 2
-        if y == "F":
-            y_max -= y_half
-        elif y == "B":
-            y_min += y_half
-
-    x_min, x_max = 0, 7
-    for x in seat[7:]:
-        x_half = x_half // 2
-        if x == "R":
-            x_min += x_half
-        elif x == "L":
-            x_max -= x_half
-
-    row = y_min
-    col = x_min
-    seat_ids.append(row * 8 + col)
-
-print(max(seat_ids))
-
+print(max(ids))
 
 # part 2
-for i, k in zip(sorted(seat_ids)[0::2], sorted(seat_ids)[1::2]):
-    if i + 1 != k:
-        print((i + k) // 2)
+it = iter(sorted(ids))
+for i, j in zip(it, it):
+    if i + 1 < j:
+        print((i + j) // 2)
