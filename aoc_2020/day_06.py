b355479f08c5ba5c573a379a4e07775d58ac9c5d
eferm <emanuel@eferm.com>
2020-12-06 01:49:46 -0600

    day 6 improve

--- aoc_2020/day_06.py
+++ aoc_2020/day_06.py
@@ -1,27 +1,16 @@
-from collections import Counter
+from functools import partial
 from _utils import *
 
 inp = get_input(2020, 6)
-# print(inp[:86])
-
 groups = inp.strip().split("\n\n")
-# print(groups[:2])
+
+
+def count(agg, group):
+    return len(agg(*[set(p) for p in group.split()]))
 
 
 # part 1
-yes = 0
-for group in groups:
-    yes += len(set(group.replace("\n", "")))
-
-print(yes)
-
+print(sum(lmap(partial(count, set.union), groups)))
 
 # part 2
-yes = 0
-for group in groups:
-    persons = group.split("\n")
-    counts = Counter(group.replace("\n", ""))
-    valid = [k for k, v in counts.items() if v == len(persons)]
-    yes += len(valid)
-
-print(yes)
+print(sum(lmap(partial(count, set.intersection), groups)))
