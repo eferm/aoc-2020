3a5c400bf3250f9b6dda13088fe13558ee9ee41a
eferm <emanuel@eferm.com>
2020-12-06 02:29:43 -0600

    day 2 cleanup

--- aoc_2020/day_02.py
+++ aoc_2020/day_02.py
@@ -3,9 +3,6 @@ from collections import Counter
 from _utils import *
 
 inp = get_input(2020, 2)
-print(inp[:78])
-print(len(inp))
-
 lines = inp.strip().split("\n")
 
 
@@ -15,19 +12,25 @@ def split(line):
 
 
 # part 1
-valid = 0
-for line in lines:
-    min_, max_, char, pw = split(line)
-    if int(min_) <= Counter(pw)[char] <= int(max_):
-        valid += 1
 
-print(valid)
+
+def validate1(line):
+    min_, max_, char, pw = split(line)
+    return int(min_) <= Counter(pw)[char] <= int(max_)
+
+
+valid = map(validate1, lines)
+print(sum(valid))
+
 
 # part 2
-valid = 0
-for line in lines:
-    a, b, char, pw = split(line)
-    if (pw[int(a) - 1] == char) != (pw[int(b) - 1] == char):
-        valid += 1
 
-print(valid)
+
+def validate2(line):
+    a, b, char, pw = split(line)
+    # xor password chars at a and b
+    return (pw[int(a) - 1] == char) != (pw[int(b) - 1] == char)
+
+
+valid = map(validate2, lines)
+print(sum(valid))
