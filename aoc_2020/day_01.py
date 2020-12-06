3c1f5d615ad960ceca120df1a1c9722b6f3ed46e
eferm <emanuel@eferm.com>
2020-12-05 19:49:59 -0600

    day 1 cleanup

--- aoc_2020/day_01.py
+++ aoc_2020/day_01.py
@@ -1,24 +1,16 @@
 import math
+from itertools import combinations
 from _utils import *
 
 inp = get_input(2020, 1)
-print(inp[:80])
-
 nums = lmap(int, inp.strip().split("\n"))
-print(nums)
-
 
 # part 1
-for i in range(len(nums)):
-    j = len(nums) - 1
-    for j in range(i + 1, len(nums)):
-        if nums[i] + nums[j] == 2020:
-            print(nums[i] * nums[j])
+for t in combinations(nums, 2):
+    if sum(t) == 2020:
+        print(math.prod(t))
 
 # part 2
-for i in range(len(nums)):
-    for j in range(i + 1, len(nums)):
-        for k in range(j + 1, len(nums)):
-            items = [nums[i], nums[j], nums[k]]
-            if sum(items) == 2020:
-                print(math.prod(items))
+for t in combinations(nums, 3):
+    if sum(t) == 2020:
+        print(math.prod(t))
