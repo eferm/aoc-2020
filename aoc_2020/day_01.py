0a9f651b122cc6f1cb22f46a815a18df209363a3
eferm <emanuel@eferm.com>
2020-12-06 03:07:40 -0600

    day 1 cleanup

--- aoc_2020/day_01.py
+++ aoc_2020/day_01.py
@@ -3,11 +3,11 @@ from itertools import combinations
 from _utils import *
 
 inp = get_input(2020, 1)
-nums = lmap(int, inp.strip().split("\n"))
+nums = lmap(int, inp.strip().split())
 
 
-def sum2020(num):
-    for t in combinations(nums, num):
+def sum2020(n):
+    for t in combinations(nums, n):
         if sum(t) == 2020:
             yield t
 
