133691be1d8f4b69519ceded84675178be205c38
eferm <emanuel@eferm.com>
2020-12-06 02:04:51 -0600

    day 5 improve

--- aoc_2020/day_05.py
+++ aoc_2020/day_05.py
@@ -3,24 +3,18 @@ from _utils import *
 inp = get_input(2020, 5)
 seats = inp.strip().split("\n")
 
-# part 1
-ids = []
-for seat in seats:
-    bits = translate(
-        {
-            "F": "0",
-            "B": "1",
-            "L": "0",
-            "R": "1",
-        },
-        seat,
-    )
-    ids.append(int(bits, 2))
 
+def seat_id(seat):
+    bits = translate({"F": "0", "B": "1", "L": "0", "R": "1"}, seat)
+    return int(bits, 2)
+
+
+ids = lmap(seat_id, seats)
+
+# part 1
 print(max(ids))
 
 # part 2
 it = iter(sorted(ids))
-for i, j in zip(it, it):
-    if i + 1 < j:
-        print((i + j) // 2)
+x = [t for t in zip(it, it) if t[0] + 1 < t[1]]
+print(sum(x.pop()) // 2)
