986bee20105dc7f4421ae675835d8533c03fb60c
eferm <emanuel@eferm.com>
2020-12-08 23:36:47 -0600

    day 9

---
+++ aoc_2020/day_09.py
@@ -0,0 +1,42 @@
+import itertools
+from _utils import *
+
+
+inp = get_input(2020, 9)
+nums = lmap(int, inp.strip().split())
+
+PRE = 25
+
+
+def subsetsum(x, seq, r=2):
+    for t in itertools.combinations(seq, r):
+        if sum(t) == x:
+            return True
+    return False
+
+
+# part 1
+
+
+def find_invalid():
+    for i in range(PRE, len(nums)):
+        if not subsetsum(nums[i], nums[i - PRE : i]):
+            return nums[i]
+
+
+invalid = find_invalid()
+print(invalid)
+
+# part 2
+
+
+def subseq(sum_to):
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            seq = nums[i:j]
+            if sum(seq) == invalid:
+                return seq
+
+
+s = subseq(invalid)
+print(min(s) + max(s))
