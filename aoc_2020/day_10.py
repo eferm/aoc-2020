e0294871038596d4fba5f85c35f42325b8fc0405
eferm <emanuel@eferm.com>
2020-12-10 12:25:00 -0600

    day 10 cleanup

--- aoc_2020/day_10.py
+++ aoc_2020/day_10.py
@@ -2,18 +2,18 @@ from _utils import *
 
 
 inp = get_input(2020, 10)
+
 adapters = lmap(int, inp.strip().split())
-adapters += [max(adapters) + 3]
+adapters = sorted(adapters) + [max(adapters) + 3]
 
 
 # part 1
 
 
 def diffs(curr, seq):
-    i, seq_ = 0, sorted(seq)
-    while i < len(seq_):
-        yield seq_[i] - curr
-        i, curr = i + 1, seq_[i]
+    for x in seq:
+        yield x - curr
+        curr = x
 
 
 d = list(diffs(0, adapters))
@@ -27,12 +27,13 @@ def graph(seq, branch=3):
     return {s: [x for x in seq if s < x <= s + branch] for s in seq}
 
 
-def dfs(counts, graph, u, t):
-    if u == t:
+def count(cache, graph, src, dst):
+    if src == dst:
         return 1
-    if u not in counts:
-        counts[u] = sum(dfs(counts, graph, c, t) for c in graph[u])
-    return counts[u]
+    if src not in cache:
+        cache[src] = sum(count(cache, graph, v, dst) for v in graph[src])
+    return cache[src]
 
 
-print(dfs({}, graph([0] + adapters), 0, max(adapters)))
+g = graph([0] + adapters)
+print(count({}, g, 0, max(adapters)))
