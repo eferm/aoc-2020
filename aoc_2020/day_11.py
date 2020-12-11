4bb6db6ef1598fdac40265b8501ebb90eb3029d2
eferm <emanuel@eferm.com>
2020-12-11 16:45:28 -0600

    day 11 part 2

--- aoc_2020/day_11.py
+++ aoc_2020/day_11.py
@@ -1,72 +1,88 @@
+import copy
+import itertools
 from _utils import *
 
 
-def print_grid_dict(grid, charmap=None, default=0):
-    # Expects a grid of (x, y) -> val
-    # Charmap maps vals to tokens for printing.  Otherwise prints str(val).
-    min_x = min([x for x, y in grid.keys()])
-    max_x = max([x for x, y in grid.keys()])
-    min_y = min([y for x, y in grid.keys()])
-    max_y = max([y for x, y in grid.keys()])
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            v = grid.get((x, y), default)
-            if charmap:
-                v = charmap[v]
-            sys.stdout.write(str(v))
-        sys.stdout.write("\n")
-
-
 inp = get_input(2020, 11)
-# inp = """L.LL.LL.LL
-# LLLLLLL.LL
-# L.L.L..L..
-# LLLL.LL.LL
-# L.LL.LL.LL
-# L.LLLLL.LL
-# ..L.L.....
-# LLLLLLLLLL
-# L.LLLLLL.L
-# L.LLLLL.LL"""
-# print(inp[:80])
+
+seats = {}
+for y, row in enumerate(inp.strip().split()):
+    for x, v in enumerate(row):
+        seats[(x, y)] = v
 
 
-def pr(m):
-    for row in m:
-        print("".join(row))
+seats = lmap(list, inp.strip().split())
+X = len(seats[0])
+Y = len(seats)
 
 
 def flat(m):
     return [x for row in m for x in row]
 
 
-seats = lmap(list, inp.strip().split())
-# pr(seats)
-# print(flat(seats))
-
-X = len(seats[0])
-Y = len(seats)
-# print(X, Y)
-
-
-def zone(x, y, seats):
-    zone = seats[max(y - 1, 0) : min(y + 2, Y)]
-    return [row[max(x - 1, 0) : min(x + 2, X)] for row in zone]
+def equals(m1, m2):
+    eq = []
+    for y in range(len(m1)):
+        for x in range(len(m1[0])):
+            eq.append(m1[y][x] == m2[y][x])
+    return all(eq)
 
 
-# pr(zone(0, 0, seats))
-# pr(zone(9, 0, seats))
-# pr(zone(0, 9, seats))
-# pr(zone(9, 9, seats))
+def adjacent(x, y, seats):
+    rows = seats[max(y - 1, 0) : min(y + 2, Y)]
+    return [row[max(x - 1, 0) : min(x + 2, X)] for row in rows]
 
-import copy
+
+def visible(x, y, seats):
+    dirs = {
+        "nw": (
+            range(max(x - 1, -1), -1, -1),
+            range(max(y - 1, -1), -1, -1),
+        ),
+        "n": (
+            itertools.repeat(x),
+            range(max(y - 1, -1), -1, -1),
+        ),
+        "ne": (
+            range(min(x + 1, X), X),
+            range(max(y - 1, -1), -1, -1),
+        ),
+        "e": (
+            range(min(x + 1, X), X),
+            itertools.repeat(y),
+        ),
+        "se": (
+            range(min(x + 1, X), X),
+            range(min(y + 1, Y), Y),
+        ),
+        "s": (
+            itertools.repeat(x),
+            range(min(y + 1, Y), Y),
+        ),
+        "sw": (
+            range(max(x - 1, -1), -1, -1),
+            range(min(y + 1, Y), Y),
+        ),
+        "w": (
+            range(max(x - 1, -1), -1, -1),
+            itertools.repeat(y),
+        ),
+    }
+    v = []
+    for k, ranges in dirs.items():
+        for x_, y_ in zip(*ranges):
+            val = seats[y_][x_]
+            v.append(val)
+            if val in ["L", "#"]:
+                break
+    return v
 
 
 def occupy(m):
     to_occupy = []
     for y, row in enumerate(m):
         for x, v in enumerate(row):
-            adjacent = flat(zone(x, y, m))
+            adjacent = visible(x, y, m)
             if v == "L" and adjacent.count("#") == 0:
                 to_occupy.append((x, y))
     n = copy.deepcopy(m)
@@ -79,8 +95,8 @@ def empty(m):
     to_empty = []
     for y, row in enumerate(m):
         for x, v in enumerate(row):
-            adjacent = flat(zone(x, y, m))
-            if v == "#" and adjacent.count("#") > 4:
+            adjacent = visible(x, y, m)
+            if v == "#" and adjacent.count("#") >= 5:
                 to_empty.append((x, y))
     n = copy.deepcopy(m)
     for x, y in to_empty:
@@ -88,24 +104,9 @@ def empty(m):
     return n
 
 
-import itertools
-
-
-def equals(m1, m2):
-    eq = []
-    for y in range(len(m1)):
-        for x in range(len(m1[0])):
-            eq.append(m1[y][x] == m2[y][x])
-    return all(eq)
-
-
 funcs = itertools.cycle([occupy, empty])
-
-
 prev = list(seats)
 curr = next(funcs)(prev)
-# pr(prev)
-# pr(curr)
 
 while not equals(prev, curr):
     prev = curr
@@ -113,24 +114,3 @@ while not equals(prev, curr):
 
 
 print(flat(curr).count("#"))
-
-
-# prev = list(seats)
-# while prev != seats:
-# for row in seats:
-#     r = list(row)
-
-
-# first_seat_empty = (i == 0) and (row[i + 1] in ["L", "."])
-# mid_seat_empty = (0 < i < len(row) - 1) and (
-#     (row[i - 1] in ["L", "."]) and (row[i + i] in ["L", "."])
-# )
-# last_seat_empty = (i == len(row) - 1) and (row[i - 1] in ["L", "."])
-# if (
-#     row[i] == "L"
-#     and first_seat_empty
-#     and mid_seat_empty
-#     and last_seat_empty
-# ):
-#     return True
-# return False
