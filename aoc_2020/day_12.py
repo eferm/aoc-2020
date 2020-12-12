77cebd35fd6600d2acebf8a38008c7396e7c3384
eferm <emanuel@eferm.com>
2020-12-12 01:27:18 -0600

    day 12 cleanup

--- aoc_2020/day_12.py
+++ aoc_2020/day_12.py
@@ -6,21 +6,29 @@ from _utils import *
 inp = get_input(2020, 12)
 instructions = inp.strip().split()
 
-dirs = {"N": (0, -1), "E": (1, 0), "S": (0, 1), "W": (-1, 0)}
+dirs = {
+    "N": (0, -1),
+    "E": (1, 0),
+    "S": (0, 1),
+    "W": (-1, 0),
+}
 
 
 # part 1
 
 
-def update_vec(curr, dir_, deg):
+def update_vec(vec, dir_, deg):
+    # (E, R, 1) -> S
     op = {"L": sub, "R": add}
-    compass = list(dirs)
-    return compass[int(op[dir_](compass.index(curr), (deg / 90)) % 4)]
+    compass = list(dirs.keys())
+    new_vec = op[dir_](compass.index(vec), (deg / 90))
+    return compass[int(new_vec) % 4]
 
 
-def update_pos(curr, dir_, num):
+def update_pos(pos, dir_, num):
+    # ((0, 0), N, 1) -> (0, -1)
     dx, dy = dirs[dir_]
-    return curr[0] + (dx * num), curr[1] + (dy * num)
+    return pos[0] + dx * num, pos[1] + dy * num
 
 
 vec = "E"
@@ -41,36 +49,29 @@ print(abs(pos[0]) + abs(pos[1]))
 # part 2
 
 
-def update_dir(curr, dir_, num):
-    dx, dy = dirs[dir_]
-    return curr[0] + (dx * num), curr[1] + (dy * num)
-
-
-def update_rot(ship, curr, dir_, num):
-    ox, oy = ship
-    px, py = curr
-    angle = math.radians(num if dir_ == "R" else 360 - num)
-
-    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+def update_rot(pnt, dir_, deg):
+    # https://stackoverflow.com/a/34374437
+    # ((1, 1), R, 90) -> (-1, 1)
+    x, y = pnt
+    angle = math.radians(deg if dir_ == "R" else 360 - deg)
+    qx = math.cos(angle) * x - math.sin(angle) * y
+    qy = math.sin(angle) * x + math.cos(angle) * y
     return round(qx), round(qy)
 
 
 pos = (0, 0)
 way = (10, -1)
 
-
 for instr in instructions:
-    print(instr)
     dir_, num = instr[0], int(instr[1:])
     if dir_ in ["N", "E", "S", "W"]:
-        way = update_dir(way, dir_, num)
+        way = update_pos(way, dir_, num)
     if dir_ in ["R", "L"]:
-        way = update_rot((0, 0), way, dir_, num)
+        way = update_rot(way, dir_, num)
     if dir_ in ["F"]:
         ew = "E" if way[0] > 0 else "W"
         ns = "S" if way[1] > 0 else "N"
-        pos = update_dir(pos, ew, num * abs(way[0]))
-        pos = update_dir(pos, ns, num * abs(way[1]))
+        pos = update_pos(pos, ew, num * abs(way[0]))
+        pos = update_pos(pos, ns, num * abs(way[1]))
 
 print(abs(pos[0]) + abs(pos[1]))
